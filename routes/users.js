dabb51d5bab1e583b870245c866899f485ce3133
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-03-02 20:24:31 -0330

    Final

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,68 @@
 const express = require('express');
+const { is } = require('express/lib/request');
 const router = express.Router(); //mini app separate from  main app 
 
 // Nest inside parent route
 router.get('/', (req, res) => {
+    req.query.name
     res.send('User List')
+});
+
+router.get('/new', (req, res) => {
+    res.render("users/new");
 });
 
 // create new user
 router.post('/', (req, res) => {
-    res.send('Create User');
+    const isValid = false;
+    if (isValid) {
+        users.push( { firstName: req.body.firstName})
+        res.redirect(`/users/${users.length -1}`)
+    } else {
+        console.log("error")
+        res.render('/users/new', { firstName: req.body.firstName });
+    }
 });
 
-// dynamic parameter
-router.get('/:id', (req, res) => {
-    req.params.id;
-    res.send(`Get user with ID ${req.params.id}`);
-});
+// Same as code below
+router.route('/:id')
+    .get((req, res) => {
+        console.log(req.user)
+        res.send(`Get user with ID ${req.params.id}`);
+    }).put((req, res) => {
+        req.params.id;
+        res.send(`Update user with ID ${req.params.id}`);
+    }).delete((req, res) => {
+        req.params.id;
+        res.send(`Delete user with ID ${req.params.id}`);
+    });
 
-router.get('/new', (req, res) => {
-    res.send('User New Form')
-});
+// // dynamic parameter - always goes after the static routes
+// router.get('/:id', (req, res) => {
+//     req.params.id;
+//     res.send(`Get user with ID ${req.params.id}`);
+// });
+
+// // Update user based on id
+// router.put('/:id', (req, res) => {
+//     req.params.id;
+//     res.send(`Update user with ID ${req.params.id}`);
+// });
 
 
+// router.delete('/:id', (req, res) => {
+//     req.params.id;
+//     res.send(`Delete user with ID ${req.params.id}`);
+// });
+
+
+const users = [ {name: "Kyle "}, {name: "Sally "}];
+
+// Whenever you find the param with 'id' run this, but it needs to see the 'next' function 
+// param is a type of middleware - runs between the request and the response 
+router.param("id", (req, res, next, id) => {
+    req.user = users[id];
+    next(); //the next piece is the route.get
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const app = express(); //creates app 
 
-app.set('view engine', 'ejs');
+// Static files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+// json request - parse JSON from the body 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log('here');
-    res.render('index', { text: 'World'});
-    // res.download('server.js');
-    // res.status(500).json({ message: "Error" });
-    // res.send('Hi'); //wont use send that often 
-});
+app.set('view engine', 'ejs');
+// app.use(logger); //middleware 
+
+// app.get('/', logger, (req, res) => {
+//     res.render('index', { text: 'World'});
+//     // res.download('server.js');
+//     // res.status(500).json({ message: "Error" });
+//     // res.send('Hi'); //wont use send that often 
+// });
 
 const userRouter = require('./routes/users');
 
 // link to path 
 app.use('/users', userRouter);
+
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+};
 
 app.listen(3000)
 
